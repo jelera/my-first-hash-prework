1c782e83c59efefebe8e4e659ffa69e598ebee5e
Jose Elera <jelera@gmail.com>
2019-03-23 02:47:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,6 +23,8 @@ def retrieval
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
+  shipping_manifest["oil paintings"]
+
 end
 
 def adding
@@ -33,12 +35,16 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
+  shipping_manifest["muskets"] = 2
+
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
+  shipping_manifest["gunpowder"] = 4
 
 
   # return the shipping_manifest hash below
+  shipping_manifest
 
 end
